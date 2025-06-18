550a3346b83228e75faf633e631cd530546a333f
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2025-06-18 18:17:49 +0900

    finish proof of concept code for task automation

--- TASK_LIST.py
+++ TASK_LIST.py
@@ -1,3 +1,8 @@
+STUDY_NAME = "Culture UX Study 1"
+
+WEBSITE_URL_1 = "https://www.figma.com/proto/tSeCgY5BvAH9BGH9HbXcKn/ICID-state_of_the_art-Austria-V0.1?node-id=1-75&starting-point-node-id=1%3A75&disable-default-keyboard-nav=1&device-frame=0&scaling=min-zoom&hide-ui=1"
+
+
 PRE_EXPERIMENT_TEXT = """
 Read this before you start the experiment:
 
@@ -13,8 +18,6 @@ PRE_EXPERIMENT_BUTTON_TEXT = "I understand"
 TASK1_TEXT = "Task 1: Find out the minimum age to acquire a Global ID."
 
 TASK_START_TEXT = "Start the task"
-
-UI_WEBSITE_1 = "https://www.figma.com/proto/tSeCgY5BvAH9BGH9HbXcKn/ICID-state_of_the_art-Austria-V0.1?node-id=1-75&starting-point-node-id=1%3A75&disable-default-keyboard-nav=1&device-frame=0&scaling=min-zoom&hide-ui=1"
 
 TASK2_TEXT = "Task 2: Find out how much it costs to use Global ID."
 
--- automation.py
+++ automation.py
@@ -1,13 +1,19 @@
+import time
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.edge.service import Service
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+import TASK_LIST
+
 
 # Setup Edge driver
 driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
 
 # Open a page
-WEBSITE = "https://uxreveal.tobii.com/en/studies/editElements/2036005350"
+WEBSITE = "https://uxreveal.tobii.com/en/studies/index"
 
 USERNAME = "naytitorn21@gmail.com"
 PASSWORD = "TobiiPassword0!"            # Password Specific - OK for public use
@@ -19,5 +25,8 @@ driver.find_element(By.NAME, "password").send_keys(PASSWORD)
 
 login_button = driver.find_element(By.NAME, "action")
 login_button.click()
+
+time.sleep(1)
+
 
 input()
