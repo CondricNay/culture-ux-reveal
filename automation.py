71dead89c737a3fb4b23778b65dea35ab14740af
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2025-06-24 16:37:02 +0900

    refactor study creation/deletion to a study manager class

--- automation.py
+++ automation.py
@@ -1,68 +1,27 @@
 import sys
 import time
+
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 import TASK_LIST
+from study_manager import StudyManager
 
 
 # Setup Edge driver
 driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+study_manager = StudyManager(driver)
 
-# Open a page
-WEBSITE = "https://uxreveal.tobii.com/en/studies/index"
-
-USERNAME = "naytitorn21@gmail.com"
-PASSWORD = "TobiiPassword0!"            # Password Specific - OK for public use
-
-driver.get(WEBSITE)
-
-driver.find_element(By.NAME, "username").send_keys(USERNAME)
-driver.find_element(By.NAME, "password").send_keys(PASSWORD)
-
-login_button = driver.find_element(By.NAME, "action")
-login_button.click()
-
-time.sleep(1)
-
-# DEBUG TODO
-# planning_link = driver.find_element(By.LINK_TEXT, "Planning")
-# planning_link.click()
-
-# delete_old_study_button = driver.find_element(By.CSS_SELECTOR, 'div.modalButton.btn-delete')
-# delete_old_study_button.click()
-
-# time.sleep(2)
-
-# delete_old_study_confirm_button = driver.find_element(By.ID, 'modal-button')
-# delete_old_study_confirm_button.click()
-# END DEBUG
-
-# Create new Study (TODO extract encapsulation)
-try: 
-    new_study_link = driver.find_element(By.CSS_SELECTOR, '#tourNewStudy a')
-    new_study_link.click()
-
-    new_study_title_field = driver.find_element(By.ID, "tourStudyTitleLabel")
-    new_study_title_field.send_keys(TASK_LIST.STUDY_NAME)
-
-    new_study_participant_dropdown = Select(driver.find_element(By.ID, "StudyNumberParticipants"))
-    new_study_participant_dropdown.select_by_value("3")
-
-    save_new_study_button = driver.find_element(By.ID, "addStudy")
-    save_new_study_button.click()
-
-except NoSuchElementException:
-    print("Can't create new study, try deleting old ones.")
-    sys.exit()
-
+# study_manager.create_study()
+# study_manager.delete_recent_study()
 time.sleep(1)
 
 # Populate with the tasks
+
+study_manager.open_website("https://uxreveal.tobii.com/en/studies/index")
 # 1) Add Pre-experiment instructions
 add_element_button = driver.find_element(By.ID, "last-add")
 add_element_button.click()
@@ -80,41 +39,41 @@ button_text_input.send_keys(TASK_LIST.PRE_EXPERIMENT_BUTTON_TEXT)
 save_button = driver.find_element(By.ID, "saveText")
 save_button.click()
 
-time.sleep(1)
+time.sleep(10000)
 
-# 2) Add Task 1
-add_element_button = driver.find_element(By.ID, "last-add")
-add_element_button.click()
+# # 2) Add Task 1
+# add_element_button = driver.find_element(By.ID, "last-add")
+# add_element_button.click()
 
-add_text_button = driver.find_element(By.CSS_SELECTOR, 'div.addContextIcon.addText')
-add_text_button.click()
+# add_text_button = driver.find_element(By.CSS_SELECTOR, 'div.addContextIcon.addText')
+# add_text_button.click()
 
-editable_div = driver.find_element(By.CSS_SELECTOR, "div.note-editable.panel-body")
-editable_div.send_keys(TASK_LIST.TASK1_TEXT)
+# editable_div = driver.find_element(By.CSS_SELECTOR, "div.note-editable.panel-body")
+# editable_div.send_keys(TASK_LIST.TASK1_TEXT)
 
-button_text_input = driver.find_element(By.ID, "TextButtonText")
-button_text_input.clear()
-button_text_input.send_keys(TASK_LIST.TASK_START_TEXT)
+# button_text_input = driver.find_element(By.ID, "TextButtonText")
+# button_text_input.clear()
+# button_text_input.send_keys(TASK_LIST.TASK_START_TEXT)
 
-save_button = driver.find_element(By.ID, "saveText")
-save_button.click()
+# save_button = driver.find_element(By.ID, "saveText")
+# save_button.click()
 
-time.sleep(1)
+# time.sleep(1)
 
-# 3) Add Website
-add_element_button = driver.find_element(By.ID, "last-add")
-add_element_button.click()
+# # 3) Add Website
+# add_element_button = driver.find_element(By.ID, "last-add")
+# add_element_button.click()
 
-add_web_button = driver.find_element(By.CSS_SELECTOR, 'div.addContextIcon.addWeb')
-add_web_button.click()
+# add_web_button = driver.find_element(By.CSS_SELECTOR, 'div.addContextIcon.addWeb')
+# add_web_button.click()
 
-website_input_field = driver.find_element(By.ID, "WebUrl")
-website_input_field.send_keys(TASK_LIST.WEBSITE_URL_1)
+# website_input_field = driver.find_element(By.ID, "WebUrl")
+# website_input_field.send_keys(TASK_LIST.WEBSITE_URL_1)
 
-save_button = driver.find_element(By.ID, "saveWeb")
-save_button.click()
+# save_button = driver.find_element(By.ID, "saveWeb")
+# save_button.click()
 
-time.sleep(1)
+# time.sleep(1)
 
 # # 4) Add Task 2
 # add_element_button = driver.find_element(By.ID, "last-add")
@@ -219,31 +178,31 @@ time.sleep(1)
 # time.sleep(1)
 
 # 10) Add Post-experiment instructions
-add_element_button = driver.find_element(By.ID, "last-add")
-add_element_button.click()
+# add_element_button = driver.find_element(By.ID, "last-add")
+# add_element_button.click()
 
-add_text_button = driver.find_element(By.CSS_SELECTOR, 'div.addContextIcon.addText')
-add_text_button.click()
+# add_text_button = driver.find_element(By.CSS_SELECTOR, 'div.addContextIcon.addText')
+# add_text_button.click()
 
-editable_div = driver.find_element(By.CSS_SELECTOR, "div.note-editable.panel-body")
-editable_div.send_keys(TASK_LIST.POST_EXPERIMENT_TEXT)
+# editable_div = driver.find_element(By.CSS_SELECTOR, "div.note-editable.panel-body")
+# editable_div.send_keys(TASK_LIST.POST_EXPERIMENT_TEXT)
 
-button_text_input = driver.find_element(By.ID, "TextButtonText")
-button_text_input.clear()
-button_text_input.send_keys("Done")
+# button_text_input = driver.find_element(By.ID, "TextButtonText")
+# button_text_input.clear()
+# button_text_input.send_keys("Done")
 
-save_button = driver.find_element(By.ID, "saveText")
-save_button.click()
+# save_button = driver.find_element(By.ID, "saveText")
+# save_button.click()
 
-time.sleep(2)
+# time.sleep(2)
 
-# Start data collection
-start_collection_button = driver.find_element(By.CSS_SELECTOR, 'button[data-studyid]')
-driver.execute_script("arguments[0].scrollIntoView(true); arguments[0].click();", start_collection_button)
+# # Start data collection
+# start_collection_button = driver.find_element(By.CSS_SELECTOR, 'button[data-studyid]')
+# driver.execute_script("arguments[0].scrollIntoView(true); arguments[0].click();", start_collection_button)
 
-time.sleep(1)
+# time.sleep(1)
 
-confirm_collection_button = driver.find_element(By.ID, "modal-button")
-confirm_collection_button.click()
+# confirm_collection_button = driver.find_element(By.ID, "modal-button")
+# confirm_collection_button.click()
 
 input()
--- study_manager.py
+++ study_manager.py
@@ -1,34 +1,60 @@
 import sys
+import time
 import TASK_LIST
 
-from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
+USERNAME = "naytitorn21@gmail.com"
+PASSWORD = "TobiiPassword0!"            # Password Specific - OK for public use
 
-# EDGE_DRIVER = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+class StudyManager():
+    def __init__(self, driver):
+        self.driver = driver
 
-# class StudyManager():
-#     def __init__(self, driver=EDGE_DRIVER):
-#         self.driver = driver
+    def open_website(self, website, username=USERNAME, password=PASSWORD):
+        self.driver.get(website)
 
-#     def create(self):
-#         try: 
-#             new_study_link = self.driver.find_element(By.CSS_SELECTOR, '#tourNewStudy a')
-#             new_study_link.click()
+        self.driver.find_element(By.NAME, "username").send_keys(username)
+        self.driver.find_element(By.NAME, "password").send_keys(password)
 
-#             new_study_title_field = self.driver.find_element(By.ID, "tourStudyTitleLabel")
-#             new_study_title_field.send_keys(TASK_LIST.STUDY_NAME)
+        login_button = self.driver.find_element(By.NAME, "action")
+        login_button.click()
 
-#             new_study_participant_dropdown = Select(self.driver.find_element(By.ID, "StudyNumberParticipants"))
-#             new_study_participant_dropdown.select_by_value("3")
+    def create_study(self):
+        self.open_website("https://uxreveal.tobii.com/en/studies/index")
 
-#             save_new_study_button = self.driver.find_element(By.ID, "addStudy")
-#             save_new_study_button.click()
+        try: 
+            new_study_link = self.driver.find_element(By.CSS_SELECTOR, '#tourNewStudy a')
+            new_study_link.click()
 
-#         except NoSuchElementException:
-#             print("Can't create new study, try deleting old ones.")
-#             sys.exit()
+            new_study_title_field = self.driver.find_element(By.ID, "tourStudyTitleLabel")
+            new_study_title_field.send_keys(TASK_LIST.STUDY_NAME)
+
+            new_study_participant_dropdown = Select(self.driver.find_element(By.ID, "StudyNumberParticipants"))
+            new_study_participant_dropdown.select_by_value("3")
+
+            save_new_study_button = self.driver.find_element(By.ID, "addStudy")
+            save_new_study_button.click()
+
+        except NoSuchElementException:
+            print("Error: Can't create new study.")
+            sys.exit()
+            
+    # TODO add delete by id
+    def delete_recent_study(self):
+        self.open_website("https://uxreveal.tobii.com/en/studies/open")
+
+        time.sleep(1)
+
+        try:
+            delete_old_study_button = self.driver.find_element(By.CSS_SELECTOR, 'div.modalButton.btn-delete')
+            delete_old_study_button.click()
+
+            delete_old_study_confirm_button = self.driver.find_element(By.ID, 'modal-button')
+            delete_old_study_confirm_button.click()
+
+        except NoSuchElementException:
+            print("Error: Can't delete study.")
+            sys.exit()
