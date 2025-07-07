070cc6c9be2328a2d111b2072fb557f59ab71474
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2025-07-07 15:55:45 +0900

    fix wrong indent causing missing question fields

--- automation.py
+++ automation.py
@@ -9,12 +9,12 @@ driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
 study_manager = StudyManager(driver)
 
 # For Debug
-# study_manager.open_website("https://uxreveal.tobii.com/en/studies/index")
+study_manager.open_website("https://uxreveal.tobii.com/en/studies/index")
 
 # study_manager.delete_recent_study()
 study_manager.create_study()
-task_pipeline = build_task_pipeline_japanese()
+task_pipeline = build_task_pipeline()
 study_manager.edit_recent_study(task_pipeline)
-# study_manager.start_new_data_collection()
+study_manager.start_new_data_collection()
 
 input("All done. Press Enter to exit.")
