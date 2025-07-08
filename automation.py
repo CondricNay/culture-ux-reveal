05d3496d2c7a33f2d09138c6a7dfd61b8d1d65d4
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2025-07-08 17:22:15 +0900

    delete system/task title

--- automation.py
+++ automation.py
@@ -11,7 +11,7 @@ study_manager = StudyManager(driver)
 # For Debug
 study_manager.open_website("https://uxreveal.tobii.com/en/studies/index")
 
-study_manager.delete_recent_study()
+# study_manager.delete_recent_study()
 # study_manager.create_study()
 # task_pipeline = build_task_pipeline()
 # study_manager.edit_recent_study(task_pipeline)
