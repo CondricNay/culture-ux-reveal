ea208ff6080c75b6f0fa381555d32501f379edab
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2025-07-15 14:55:07 +0900

    updaye task title

--- automation.py
+++ automation.py
@@ -12,9 +12,9 @@ study_manager = StudyManager(driver)
 study_manager.open_website("https://uxreveal.tobii.com/en/studies/index")
 
 # study_manager.delete_recent_study()
-# study_manager.create_study()
-# task_pipeline = build_task_pipeline()
-# study_manager.edit_recent_study(task_pipeline)
-# study_manager.start_new_data_collection()
+study_manager.create_study()
+task_pipeline = build_task_pipeline()
+study_manager.edit_recent_study(task_pipeline)
+study_manager.start_new_data_collection()
 
 input("All done. Press Enter to exit.")
